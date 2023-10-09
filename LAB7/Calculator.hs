ed988854c5a8bdc9a1955f7339602b3f3c75daff
gautamop01 <gkmsaypur99@gmail.com>
2023-10-09 19:54:40 +0530

    LAB 7

--- LAB7/Calculator.hs
+++ LAB7/Calculator.hs
@@ -1,33 +1,24 @@
-import Data.List.Split (splitOn)
-import Control.Monad (unless)
-
 -- Function to evaluate a postfix expression
-evaluatePostfix :: String -> Maybe Int
-evaluatePostfix expr = evalHelper (words expr) []
-
-evalHelper :: [String] -> [Int] -> Maybe Int
-evalHelper [] [result] = Just result
-evalHelper [] _ = Nothing
-evalHelper (token:tokens) stack
-    | all isDigit token = evalHelper tokens (read token : stack)
-    | length stack >= 2 = case token of
-        "+" -> evalHelper tokens $ (stack !! 1 + stack !! 0) : drop 2 stack
-        "-" -> evalHelper tokens $ (stack !! 1 - stack !! 0) : drop 2 stack
-        "*" -> evalHelper tokens $ (stack !! 1 * stack !! 0) : drop 2 stack
-        "/" -> evalHelper tokens $ (stack !! 1 `div` stack !! 0) : drop 2 stack
-        _ -> Nothing
-    | otherwise = Nothing
+evaluatePostfix :: String -> Int
+evaluatePostfix expr = head $ foldl evalStep [] (words expr)
+  where
+    evalStep :: [Int] -> String -> [Int]
+    evalStep stack token
+      | token `elem` ["+", "-", "*", "/"] = do
+          let val2 = head stack
+          let val1 = head (tail stack)
+          case token of
+            "+" -> (val1 + val2) : (tail (tail stack))
+            "-" -> (val1 - val2) : (tail (tail stack))
+            "*" -> (val1 * val2) : (tail (tail stack))
+            "/" -> (div val1 val2) : (tail (tail stack))
+      | otherwise = (read token :: Int) : stack
 
 main :: IO ()
 main = do
-    putStrLn "Enter a postfix expression (separate operators and operands by spaces):"
+    putStrLn "Enter a postfix expression (separated by spaces):"
     input <- getLine
     let result = evaluatePostfix input
-    case result of
-        Just value -> putStrLn $ "Result: " ++ show value
-        Nothing -> putStrLn "Invalid postfix expression."
-    
-    putStrLn "Do you want to evaluate another expression? (y/n)"
-    choice <- getLine
-    unless (choice /= "y") main
+    putStrLn $ "Result: " ++ show result
+    main -- Continue the interactive loop
 
